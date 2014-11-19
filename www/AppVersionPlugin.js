5f0ea9c3a2e8c23c69410478491189dc92174dd1
Matias Singers <mail@matiassingers.com>
2014-11-19 14:20:16 +0800

    Create #getVersionName() and #getVersionCode() methods

--- www/AppVersionPlugin.js
+++ www/AppVersionPlugin.js
@@ -1,13 +1,25 @@
 // Returns a jQuery or AngularJS deferred object, or pass a success and fail callbacks if you don't want to use jQuery or AngularJS
-var getAppVersion = function (success, fail) {
-  var toReturn, deferred;
+var AppVersion = function() {};
+
+AppVersion.prototype.getVersionName = function(success, fail) {
+  return promiseWrapper(success, fail, 'getVersionName');
+};
+
+AppVersion.prototype.getVersionCode = function(success, fail) {
+  return promiseWrapper(success, fail, 'getVersionCode');
+};
+
+function promiseWrapper(success, fail, method) {
+  var toReturn;
+
+  var deferred;
   if ((typeof success) === 'undefined') {
     if(window.jQuery){
       deferred = jQuery.Deferred();
       toReturn = deferred;
     } else if(window.angular){
-      var injector = angular.injector(["ng"]);
-      var $q = injector.get("$q");
+      var injector = angular.injector(['ng']);
+      var $q = injector.get('$q');
       deferred = $q.defer();
       toReturn = deferred.promise
     } else {
@@ -17,8 +29,8 @@ var getAppVersion = function (success, fail) {
     fail = deferred.reject;
   }
   // 5th param is NOT optional. must be at least empty array
-  cordova.exec(success, fail, "AppVersion", "getVersionNumber", []);
+  cordova.exec(success, fail, 'AppVersion', method, []);
   return toReturn;
-};
+}
 
-module.exports = getAppVersion;
+module.exports = AppVersion;
