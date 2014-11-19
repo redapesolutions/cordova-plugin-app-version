618da704894ca1e3cad206350f50045136969f63
Matias Singers <mail@matiassingers.com>
2014-11-19 14:19:50 +0800

    Add versionCode and versionName support for Android

--- src/android/AppVersion.java
+++ src/android/AppVersion.java
@@ -13,11 +13,16 @@ public class AppVersion extends CordovaPlugin {
 	@Override
         public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
-		try {	
-			if (action.equals("getVersionNumber")) {
+		try {
+			if (action.equals("getVersionName")) {
 				PackageManager packageManager = this.cordova.getActivity().getPackageManager();
 				callbackContext.success(packageManager.getPackageInfo(this.cordova.getActivity().getPackageName(), 0).versionName);
-			return true;
+				return true;
+			}
+			if (action.equals("getVersionCode")) {
+				PackageManager packageManager = this.cordova.getActivity().getPackageManager();
+				callbackContext.success(packageManager.getPackageInfo(this.cordova.getActivity().getPackageName(), 0).versionCode);
+				return true;
 			}
 			return false;
 		} catch (NameNotFoundException e) {
