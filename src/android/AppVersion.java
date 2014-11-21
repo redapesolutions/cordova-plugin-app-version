8b22604c9fa4c5f530f07fe4cb9ace135c24113a
yujinlim <adamlyj89@gmail.com>
2014-11-21 17:07:15 +0800

    update dependency and catch clause

--- src/android/AppVersion.java
+++ src/android/AppVersion.java
@@ -9,31 +9,33 @@ import org.json.JSONObject;
 
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageInfo;
 
 public class AppVersion extends CordovaPlugin {
-	@Override
+  @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         boolean result = false;
         
         PackageManager packageManager = this.cordova.getActivity().getPackageManager();
-        PackageInfo packageInfo = packageManager.getPackageInfo(this.cordova.getActivity().getPackageName(), 0);
+        PackageInfo packageInfo = null;
+        try {
+          packageInfo = packageManager.getPackageInfo(this.cordova.getActivity().getPackageName(), 0);
+        } catch (NameNotFoundException e1) {
+          // TODO Auto-generated catch block
+          e1.printStackTrace();
+        }
 
-		try {
-			if (action.equals("getVersionName")) {
-				callbackContext.success(packageInfo.versionName);
-				return true;
-			}
+        if (action.equals("getVersionName")) {
+          callbackContext.success(packageInfo.versionName);
+          return true;
+        }
 
-			if (action.equals("getVersionCode")) {
-				callbackContext.success(packageInfo.versionCode);
-				return true;
-			}
+        if (action.equals("getVersionCode")) {
+          callbackContext.success(packageInfo.versionCode);
+          return true;
+        }
 
-			return false;
-		} catch (NameNotFoundException e) {
-			callbackContext.success("N/A");
-			return true;
-		}
-	}
-
+        return false;
+      }
+  }
 }
